db0241c211255459b4b877c367a60e0828ac153b
Franco C Sanchez <francocsanchez@gmail.com>
2022-03-12 19:43:33 -0300

    Install Pug

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,3 @@
 exports.index = (req, res) => {
-    res.send('Welcome to express of Controller')
+    res.render('./home/index')
 }
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 const routesIndex = require('./routes');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views'));
 
 app.use('/', routesIndex())
 
