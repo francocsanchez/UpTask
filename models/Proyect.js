79eeff581b808f8838989972f49afc1f4a2dfe35
Franco C Sanchez <francocsanchez@gmail.com>
2022-03-13 19:50:27 -0300

    Generate URL unique

--- models/Proyect.js
+++ models/Proyect.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const slug = require('slug');
+const shortid = require('shortid');
 const db = require('../config/db');
 
 const Proyect = db.define('proyects', {
@@ -9,6 +11,13 @@ const Proyect = db.define('proyects', {
     },
     name: Sequelize.STRING(60),
     url: Sequelize.STRING(150)
+}, {
+    hooks: {
+        beforeCreate(proyect) {
+            const url = slug(proyect.name).toLowerCase();
+            proyect.url = `${url}-${shortid.generate()}`;
+        }
+    }
 });
 
 module.exports = Proyect;
