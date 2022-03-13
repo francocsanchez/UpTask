4f9d9fd712ce3dba00f97aa22b35a50fa2e84cd1
Franco C Sanchez <francocsanchez@gmail.com>
2022-03-13 20:28:51 -0300

    Create helper vardump of javascript

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const routesIndex = require('./routes');
+const helpers = require('./helpers');
 
 // Conexion DB - Generacion de Tablas
 const db = require('./config/db');
@@ -16,6 +17,10 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './views'));
+app.use((req, res, next) => {
+    res.locals.vardump = helpers.vardump
+    next();
+})
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routesIndex())
