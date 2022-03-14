99d895fc728a9b8c84b6ce7031d1a3d8b805f690
Franco C Sanchez <francocsanchez@gmail.com>
2022-03-14 13:52:25 -0300

    Routing proyect

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,24 +1,32 @@
 const Proyect = require('../models/Proyect');
 
-exports.index = async(req, res) => {
+exports.index = async (req, res) => {
     const proyects = await Proyect.findAll();
-    res.render('./home/index', { title: 'Home' , proyects})
+    res.render('./home/index', { title: 'Home', proyects })
 }
 
-exports.newProyect = (req, res) => {
-    res.render('./home/newProyect', { title: 'Nuevo Proyecto' })
+exports.newProyect = async (req, res) => {
+    const proyects = await Proyect.findAll();
+    res.render('./home/newProyect', { title: 'Nuevo Proyecto', proyects })
 }
 
 exports.addProyect = async (req, res) => {
+    const proyects = await Proyect.findAll();
     const { name } = req.body
 
     let errors = [];
     if (!name) { errors.push({ 'texto': 'Agregar un nombre' }) }
 
     if (errors.length > 0) {
-        res.render('./home/newProyect', { title: 'Nuevo Proyecto', errors })
+        res.render('./home/newProyect', { title: 'Nuevo Proyecto', errors, proyects })
     } else {
         const proyect = await Proyect.create({ name });
         res.redirect('/');
     }
+}
+
+exports.showProyect = async (req, res) => {
+    const proyects = await Proyect.findAll();
+    const proyect = await Proyect.findOne({ where: { url: req.params.url } })
+    res.render('./home/proyect', { title: 'Proyecto', proyects, proyect })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const { body } = require('express-validator');
+const { route } = require('express/lib/application');
 const router = express.Router();
+
+const validation = body('name').not().isEmpty().trim().escape();
 
 const homeController = require('../controllers/homeController');
 
 module.exports = function () {
     router.get('/', homeController.index)
 
-    //Rutas de creacion de proyecto
+    //Rutas  de proyecto
     router.get('/newProyect', homeController.newProyect)
-    router.post('/newProyect',
-        body('name').not().isEmpty().trim().escape(),
-        homeController.addProyect)
+    router.post('/newProyect', validation, homeController.addProyect)
+    router.get('/proyects/:url', homeController.showProyect)
 
     return router;
 }
