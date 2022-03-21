5ec292787ea5f6f03ce112c6d8bce319520a16c0
Franco C Sanchez <francocsanchez@gmail.com>
2022-03-20 23:22:25 -0300

    Create task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,3 +1,15 @@
-exports.addTask =  (req, res) => {
-    res.send('tarea agregada');
+const Proyect = require('../models/Proyect');
+const Task = require('../models/Task');
+
+exports.addTask = async (req, res, next) => {
+    const proyect = await Proyect.findOne({ where: { url: req.params.url } })
+    const { task } = req.body;
+    const status = 0;
+    const proyectId = proyect.id;
+
+    const result = await Task.create({ task, status, proyectId });
+
+    !result ? next() : '';
+
+    res.redirect(`/proyects/${req.params.url}`);
 }
