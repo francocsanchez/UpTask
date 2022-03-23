027b2848422d09fb8a1b4a9312bbdfca3a616548
Franco C Sanchez <francocsanchez@gmail.com>
2022-03-23 20:01:18 -0300

    update fakerController

--- controllers/fakerController.js
+++ controllers/fakerController.js
@@ -8,18 +8,21 @@ exports.faker = async (req, res) => {
         let randomEmail = faker.internet.email();
         let randomPassword = faker.name.findName();
         let result = await User.create({ email: randomEmail, password: randomPassword })
+    }
 
-        for (p = 1; p < 3; p++) {
-            let randomUrl = faker.internet.url();
-            let randomName = faker.name.findName();
-            let result = await Proyect.create({ name: randomName, url: randomUrl, userId: i })
+    for (p = 1; p < 3; p++) {
+        let randomUrl = faker.internet.url();
+        let randomName = faker.name.findName();
+        let result = await Proyect.create({ name: randomName, url: randomUrl, userId: p })
+    }
 
-            for (t = 0; t <= 6; t++) {
-                let randomTask = faker.vehicle.vehicle();
-                let randomStatus = faker.datatype.boolean();
-                let result = await Task.create({ task: randomTask, status: randomStatus, proyectId: p })
-            }
+    for (t = 1; t < 3; t++) {
+        for (p = 0; p < 5; p++) {
+            let randomTask = faker.vehicle.vehicle();
+            let randomStatus = faker.datatype.boolean();
+            let result = await Task.create({ task: randomTask, status: randomStatus, proyectId: t })
         }
     }
+
     res.send('Datos creados');
 }
