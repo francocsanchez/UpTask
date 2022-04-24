c4f7f1fa9c162b48b23deef3a1ed72dfb6fa98fd
Franco C Sanchez <francocsanchez@gmail.com>
2022-04-23 21:49:40 -0300

    Config ENV file

--- config/db.js
+++ config/db.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('uptask', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+require('dotenv').config({path: 'variables.env'})
+const db = new Sequelize(
+    process.env.BD_NAME,
+    process.env.BD_USER,
+    process.env.BD_PASS,
+    {
+        host: process.env.BD_HOST,
+        dialect: 'mysql'
+    });
 
 module.exports = db;
